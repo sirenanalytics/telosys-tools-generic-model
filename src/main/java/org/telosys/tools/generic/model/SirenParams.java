42748620ea9278f02b6de64068c353a7f0cd9b1a
Aantoine Samaha <antoine.samaha@01barmaja.com>
2023-06-09 10:57:38 +0300

    Adding Custom Annotations

--- src/main/java/org/telosys/tools/generic/model/SirenParams.java
+++ src/main/java/org/telosys/tools/generic/model/SirenParams.java
@@ -5,10 +5,19 @@ import java.util.HashMap;
 public class SirenParams {
 	
 	public static final String NoBlanks = "NoBlanks";
+	public static final String NoNulls = "NoNulls";
 	public static final String ArabicOrEnglishOnlyConstraint = "ArabicOrEnglishOnlyConstraint";
+	public static final String NumbersGreaterThanZeroOnlyConstraint = "NumbersGreaterThanZeroOnlyConstraint";
+	public static final String LebaneseMobileConstraint = "LebaneseMobileConstraint";
+	public static final String EmailConstraint = "EmailConstraint";
+	public static final String MinMaxSize = "MinMaxSize";
+	public static final String MinSize = "MinSize";
+	public static final String MaxSize = "MaxSize";
 	
 	public static final String Exists = "Exists";
 	public static final String Message = "Message";
+	public static final String Min = "Min";
+	public static final String Max = "Max";
 	
 	private HashMap<String, Object> sirenParams;
 	
